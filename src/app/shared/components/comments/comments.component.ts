388e86f1dce83c469fcd7feb48e82566c31c6850
Mostafa Sadiki <mostafa.sadiki@sogeti.com>
2023-02-10 17:38:58 +0100

    component réutilisable

---
+++ src/app/shared/components/comments/comments.component.ts
@@ -0,0 +1,32 @@
+import { outputAst } from '@angular/compiler';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Comment } from 'src/app/core/models/comment.model';
+
+@Component({
+  selector: 'app-comments',
+  templateUrl: './comments.component.html',
+  styleUrls: ['./comments.component.scss']
+})
+export class CommentsComponent implements OnInit {
+
+  @Input() comments!: Comment[];
+  @Output() newComment = new EventEmitter<string>();
+
+  commentCtrl!: FormControl;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.commentCtrl = this.formBuilder.control('', [Validators.required, Validators.minLength(10)])
+  }
+
+  onLeaveComment(): void {
+    if (this.commentCtrl.invalid) {
+      return;
+    }
+    this.newComment.emit(this.commentCtrl.value);
+    this.commentCtrl.reset();
+  }
+
+}
